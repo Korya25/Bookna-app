dc4c9cbde0326783db0908474ae4d63aaaad0d42
Korya25 <enthrj3@gmail.com>
2025-04-07 20:57:58 +0200

    add domain layer for author

---
+++ lib/features/books/domain/entities/author.dart
@@ -0,0 +1,50 @@
+/// Represents an author entity with relevant details.
+class Author {
+  final String? key;
+  final String? name;
+  final List<String>? alternateNames;
+  final String? birthDate;
+  final String? deathDate;
+  final List<String>? topSubjects;
+  final String? topWork;
+  final String? type;
+  final int? workCount;
+  final double? ratingsAverage;
+  final double? ratingsSortable;
+  final int? ratingsCount;
+  final int? ratingsCount1;
+  final int? ratingsCount2;
+  final int? ratingsCount3;
+  final int? ratingsCount4;
+  final int? ratingsCount5;
+  final int? wantToReadCount;
+  final int? alreadyReadCount;
+  final int? currentlyReadingCount;
+  final int? readingLogCount;
+  final String? version;
+
+  Author({
+    required this.key,
+    required this.name,
+    this.alternateNames,
+    this.birthDate,
+    this.deathDate,
+    required this.topSubjects,
+    required this.topWork,
+    required this.type,
+    required this.workCount,
+    required this.ratingsAverage,
+    required this.ratingsSortable,
+    required this.ratingsCount,
+    required this.ratingsCount1,
+    required this.ratingsCount2,
+    required this.ratingsCount3,
+    required this.ratingsCount4,
+    required this.ratingsCount5,
+    required this.wantToReadCount,
+    required this.alreadyReadCount,
+    required this.currentlyReadingCount,
+    required this.readingLogCount,
+    required this.version,
+  });
+}
--- lib/features/books/domain/entities/book.dart
+++ lib/features/books/domain/entities/book.dart
@@ -1,31 +1,15 @@
-import 'package:hive/hive.dart';
-part 'book.g.dart';
-
-@HiveType(typeId: 0)
 class Book {
-  @HiveField(0)
   final String? bookId;
-  @HiveField(1)
   final String? title;
-  @HiveField(2)
   final List<String>? authors;
-  @HiveField(3)
   final String? publishedDate;
-  @HiveField(4)
   final String? description;
-  @HiveField(5)
   final num? pageCount;
-  @HiveField(6)
   final num? price;
-  @HiveField(7)
   final num? averageRating;
-  @HiveField(8)
   final num? ratingsCount;
-  @HiveField(9)
   final String thumbnail;
-  @HiveField(10)
   final List<String>? categories;
-  @HiveField(11)
   final String? previewLink;
 
   Book({
